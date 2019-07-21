10bae7f2fcb03ae15a6bf5864ac401a066adb00d
rlane85 <44262357+rlane85@users.noreply.github.com>
2019-07-21 15:25:08 -0400

    0.3 update
    added style & button to switch from upcoming to previous

--- datatocells.js
+++ datatocells.js
@@ -1,18 +1,87 @@
-jQuery(document).ready(function ($) {
-console.log();
-        var column = 1;
-        var row = 2;
-       $('#tablepress-2 > tbody > tr.row-' + row + ' > td.column-' + column).text(launchData[column-1][row-2]);
+function jsUcfirst(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
-       for (row = 2; row <= 30; row++) {
-            for (column = 1; column <= 9; column++) {
-                if (column != 9){
-                    $('#tablepress-2 > tbody > tr.row-' + row + ' > td.column-' + column).text(launchData[column-1][row - 2]);
-                }
-                else{
-                    $('#tablepress-2 > tbody > tr.row-' + row + ' > td.column-' + column).html('<a href = "' + launchData[column-1][row - 2] + '" target="_blank"> More Info </a>');
-                }
-            }
-        }
+//functions to run when document is finished loading
+jQuery(document).ready(function () {
+    jQuery("#launchbutton").css({
+        "font-size": "24px",
+        "text-align": "center"
+
+    });
+    jQuery("#buttondiv").css({
+        "text-align": "center"
+
+    });
+
+
+    dataToCells(launchdata);
+});
+
+//button click function
+jQuery("#launchbutton").click(function () {
+
+    table = jQuery("#launchbutton").text();
+    if (table == "Upcoming") {
+        dataToCells(launchdata);
+
+        jQuery(this).button({
+            label: jsUcfirst(prevlaunchdata[15].type.toString())
+        });
+        jQuery("#titlediv").html('<h2>' + jsUcfirst(table) + " Launches</h2>");
+    } else {
+        jQuery(this).button({
+            label: jsUcfirst(launchdata[15].type.toString())
+        });
+        dataToCells(prevlaunchdata);
+        jQuery("#titlediv").html('<h2>' + jsUcfirst(table) + " Launches</h2>");
+
+    }
+
 
 });
+
+//main function to assign data to cells
+function dataToCells(data) {
+    var slug = 8;
+    var start = 9;
+    var end = 10;
+    var mission = 11;
+    var spacecraft = 12;
+    var pad = 13;
+    var color = 14;
+    var column = 1;
+    var row = 2;
+    var table = launchdata[15].type;
+
+    //assign title correct text
+    jQuery("#titlediv").html(jsUcfirst('<h2>' + jsUcfirst(table.toString()) + " Launches</h2>"))
+        .css("text-align", "center");
+
+    //loop through cells
+    for (row = 2; row <= 30; row++) {
+        for (column = 1; column <= 9; column++) {
+            if (column == 1) { //launch window & hyperlink to spacelaunch.me 
+                jQuery('#tablepress-launches > tbody > tr.row-' + row + ' > td.column-' + column)
+                    .html('<a href = "' + data[slug][row - 2] + '" target="_blank" title = "Launch Window: ' + data[start][row - 2] + 'thru ' + data[end][row - 2] + '">' + data[column - 1][row - 2] + '</a>');
+            } else if (column == 3) { //mission description & colorize from status.
+                jQuery('#tablepress-launches > tbody > tr.row-' + row + ' > td.column-' + column)
+                    .html('<p id = "p" title = "' + data[mission][row - 2] + '">' + data[column - 1][row - 2] + '</p>')
+                    .css({
+                        "background-color": data[color][row - 2],
+                        "color": "black"
+                    });
+            } else if (column == 7) { //spacecraft description
+                jQuery('#tablepress-launches > tbody > tr.row-' + row + ' > td.column-' + column)
+                    .html('<p id = "p" title = "' + data[spacecraft][row - 2] + '">' + data[column - 1][row - 2] + '</p>');
+            } else if (column == 5) { //pad wiki
+                jQuery('#tablepress-launches > tbody > tr.row-' + row + ' > td.column-' + column)
+                    .html('<a href = "' + data[pad][row - 2] + '" target="_blank">' + data[column - 1][row - 2] + '</a>');
+            } else { //no other markup
+                jQuery('#tablepress-launches > tbody > tr.row-' + row + ' > td.column-' + column)
+                    .html(data[column - 1][row - 2]);
+            }
+        }
+    }
+
+}
--- launch.php
+++ launch.php
@@ -2,7 +2,7 @@
 
 /**
  * @package launches
- * @version 0.1
+ * @version 0.3
  */
 /*
 Plugin Name: Rocket Launches
@@ -13,49 +13,96 @@ Version: 0.1
 Author URI: https://ahomeconnected.com
 */
 
+require_once plugin_dir_path(__FILE__) . '/vendor/autoload.php';
 
-    
-
-
-require_once plugin_dir_path(__FILE__).'/vendor/autoload.php';
 use Flow\JSONPath\JSONPath;
-$nlSearch = 'Space Launch Complex FL';
-$nlQueryLimit = '30';
-$nlUrl = 'https://spacelaunchnow.me/api/3.3.0/launch/upcoming?limit='.$nlQueryLimit.'&search='.$nlSearch;  
-$nlResponse = wp_remote_get($nlUrl);
-$nlBody =  json_decode($nlResponse['body']);
-function nlPath($metric, $nlBody) 
-{
-    return json_decode(json_encode((new JSONPath($nlBody))->find("$.results[*].$metric")));
-}
-$nlNet = nlPath("net", $nlBody);
-$nlSlug = nlPath("slug", $nlBody);
 
-foreach ($nlNet as $key => $value)
-    {$nlNet[$key] = date_i18n("g:ia n/j ", strtotime($nlNet[$key]));}
-unset($value);
-$launchData = array
-(
-    $nlNet,                                                                 
-
-    nlPath("rocket.configuration.launch_service_provider", $nlBody),
-   
-    nlPath("mission.name", $nlBody),
-    nlPath("mission.type", $nlBody),
-    nlPath("pad.name", $nlBody),
-    nlPath("rocket.configuration.name", $nlBody),
-    nlPath("rocket.spacecraft_stage.spacecraft.name", $nlBody),
-    nlPath("rocket.spacecraft_stage.destination", $nlBody),
-
-    nlPath("slug", $nlBody),
-
-);
-
-add_action('wp_enqueue_scripts', function (){
-    global $launchData;
-    wp_register_script('datatocells', plugin_dir_url(__FILE__).'datatocells.js', array('jquery'),null, true);
+add_action('wp_enqueue_scripts', function () {
+    wp_register_script('datatocells', plugin_dir_url(__FILE__) . 'datatocells.js', array('jquery'), null, true);
+    wp_enqueue_script('jquery-ui-widget');
+    wp_enqueue_script('jquery-ui-button');
     wp_enqueue_script('datatocells');
-    wp_localize_script('datatocells', 'launchData', $launchData);
-
 });
+function launchdata($atts, $content = null)
+{
 
+    $nlSearch = 'Space Launch Complex FL';
+    $nlQueryLimit = '30';
+    $nlUrl = 'https://spacelaunchnow.me/api/3.3.0/launch/' . $atts['type'] . '?limit=' . $nlQueryLimit . '&search=' . $nlSearch;
+    $nlResponse = wp_remote_get($nlUrl);
+    $nlBody =  json_decode($nlResponse['body']);
+    if (!function_exists('nlPath')) {
+        function nlPath($metric, $nlBody)
+        {
+            return json_decode(json_encode((new JSONPath($nlBody))->find("$.results[*].$metric")));
+        }
+    }
+    $nlNet = nlPath("net", $nlBody);
+    $nlStart = nlPath("window_start", $nlBody);
+    $nlEnd = nlPath("window_end", $nlBody);
+    $nlStatus = nlPath("status.id", $nlBody);
+
+    //formatting dates on each index for the items that return datetimes
+    foreach ($nlNet as $key => $value) {
+        $nlNet[$key] = date_i18n("g:ia n/j 'y ", strtotime($nlNet[$key]));
+    }
+    unset($value);
+    foreach ($nlStart as $key => $value) {
+        $nlStart[$key] = date_i18n("g:ia n/j ", strtotime($nlStart[$key]));
+    }
+    unset($value);
+    foreach ($nlEnd as $key => $value) {
+        $nlEnd[$key] = date_i18n("g:ia n/j ", strtotime($nlEnd[$key]));
+    }
+    unset($value);
+
+    //assigning a color as string to each status id
+    foreach ($nlStatus as $key => $value) {
+        if ($nlStatus[$key] == 1) //"go"
+        {
+            $nlStatus[$key] = "lightblue";
+        } else if ($nlStatus[$key] == 3) //"success"
+        {
+            $nlStatus[$key] = "lightgreen";
+        } else if ($nlStatus[$key] == 6) //"in flight"
+        {
+            $nlStatus[$key] = "pink";
+        } else {
+            $nlStatus[$key] = "white";
+        }
+    }
+    unset($value);
+    global $launchData;
+
+    $launchData = array(
+        $nlNet,
+
+        nlPath("rocket.configuration.launch_service_provider", $nlBody),
+
+        nlPath("mission.name", $nlBody),
+        nlPath("mission.type", $nlBody),
+        nlPath("pad.name", $nlBody),
+        nlPath("rocket.configuration.name", $nlBody),
+        nlPath("rocket.spacecraft_stage.spacecraft.name", $nlBody),
+        nlPath("rocket.spacecraft_stage.destination", $nlBody),
+
+        //info for tooltips and styling at the end
+
+        nlPath("slug", $nlBody),
+        $nlStart,
+        $nlEnd,
+        nlPath("mission.description", $nlBody),
+        nlPath("rocket.spacecraft_stage.spacecraft.description", $nlBody),
+        nlPath("pad.wiki_url", $nlBody),
+        $nlStatus,
+        $atts
+
+    );
+    if ($atts["type"] == "upcoming") {
+        wp_localize_script('datatocells', 'launchdata', $launchData);
+    } else {
+        wp_localize_script('datatocells', 'prevlaunchdata', $launchData);
+    }
+
+}
+add_shortcode('launch', 'launchdata');
